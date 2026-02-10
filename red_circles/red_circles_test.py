b465a6bf25885a42aecb33416b9110c485359d3d
lukekremer6 <lukekremer6@gmail.com>
2026-02-10 10:15:09 -0500

    Updated red_circles_test.py

--- red_circles/red_circles_test.py
+++ red_circles/red_circles_test.py
@@ -1,24 +1,11 @@
 # Demonstrates red circle detection on sample images.
 
 from red_circles import find_red_circles
+import os
 
 if __name__ == "__main__":
-    test_images = [
-        "test_images/image1.png",
-        "test_images/image2.png",
-        "test_images/image9.png",
-        "test_images/image13.png",
-        "test_images/image35.png",
-        "test_images/rand2.png",
-        "test_images/rand3.jpeg",
-        "test_images/rand4.jpeg",
-        "test_images/rand5.jpeg",
-        "test_images/rand6.jpeg",
-        "test_images/rand7.jpg",
-        "test_images/rand8.jpeg",
-        "test_images/rand9.jpeg",
-        "test_images/The_Death_Star.png",
-    ]
-
-    for image in test_images:
-        find_red_circles(image)
+    usb_path = r"D:\sampleimages"
+    for filename in os.listdir(usb_path):
+        if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
+            path = os.path.join(usb_path, filename).replace("\\", "/")
+            find_red_circles(path)
